9031143059cea306dc2586ce1a725a632bfa48b9
Erick William <erickwilliamew@gmail.com>
2026-05-24 14:10:23 -0300

    Add page objects

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,36 +1,45 @@
 import userData from '../fixtures/userData.json'
+import LoginPage from '../pages/loginPage'
+import DashboardPage from '../pages/dashboardPage'
+import MenuPage from '../pages/menuPage.js'
+
+const loginPage = new LoginPage()
+const dashboardPage = new DashboardPage()
+const menuPage = new MenuPage()
+
+
 
 describe('Orange HRM tests', () => {
 
   const selectorsList = {
-    usernameField: "[name='username']",
-    passwordField: '[name="password"]',
-    loginButton: "[type='submit']",
-    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
-    dashboardGrid: ".orangehrm-dashboard-grid",
-    wrongCredentialAlert: "[role='alert']",
+   
+   
     myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
     firstNameField: "[name='firstName']",
     middleNameField: "[name='middleName']",
     lastNameField: "[name='lastName']",
     genericField: ".oxd-input--active",
     dateField: "[placeholder='yyyy-dd-mm']",
+    genericCombobox: ".oxd-select-text--arrow",
+    secondItemCombobox: ".oxd-select-dropdown > :nth-child(2)",
+    thirdItemCombobox: ".oxd-select-dropdown > :nth-child(3)",
     dateCloseButton: ".--close",
     submitButton: "[type='submit']"
     
-   
+
 
   }
 
   it.only('User Info Update - Success', () => {
- 
-    cy.visit('/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
-    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
-    cy.get(selectorsList.loginButton).click()
-    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorsList.dashboardGrid)
-    cy.get(selectorsList.myInfoButton).click()
+    loginPage.accessLoginPage()
+    loginPage.loginWithAnyUser(userData.userSuccess.username, userData.userSuccess.password)
+
+    dashboardPage.checkDashboardPage()
+
+    menuPage.accessMyInfo()
+    
+
+   
     cy.get(selectorsList.firstNameField).clear().type('FirstNametest')
     cy.get(selectorsList.middleNameField).clear().type('MiddleNametest')
     cy.get(selectorsList.lastNameField).clear().type('LastNametest')
@@ -38,12 +47,18 @@ describe('Orange HRM tests', () => {
     cy.get(selectorsList.genericField).eq(4).clear().type('111111')
     cy.get(selectorsList.genericField).eq(5).clear().type('1234')
     cy.get(selectorsList.genericField).eq(6).clear().type('2025-03-10')
-     cy.get(selectorsList.dateCloseButton).click()
-    cy.get(selectorsList.genericField).eq(7).clear().type('1992-06-15')
-     cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.genericField).eq(7).clear({ force: true }).type('1992-06-15')
+    cy.get(selectorsList.dateCloseButton).click()
     cy.get(selectorsList.genericField).eq(8).clear().type('TesteIField')
-    cy.get(selectorsList.submitButton).eq(0).click()
-    cy.get('.oxd-toast-close')
+    cy.get(selectorsList.submitButton).eq(0).click({ force: true })
+    // cy.get('.oxd-toast-close')
+     cy.get('.oxd-toast-close', { timeout: 10000 }).should('be.visible').click();
+
+    cy.get(selectorsList.genericCombobox).eq(0).click({ force: true })
+    cy.get(selectorsList.secondItemCombobox).click()
+    cy.get(selectorsList.genericCombobox).eq(1).click({ force: true })
+    cy.get(selectorsList.thirdItemCombobox).click()
     
     
   })
